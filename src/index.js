2b6b0fe792c89b0fd2080734fc279e8440ef5d2c
pmfnascimento <pmfnascimento@gmail.com>
2021-02-09 22:01:22 +0000

    Simple List

--- src/index.js
+++ src/index.js
@@ -3,48 +3,34 @@ import ReactDOM from 'react-dom'
 
 // Css
 import './index.css'
-const firstBook = {
-  author: 'Hardcover – May 26, 2020',
-  title: 'Breath: The New Science of a Lost Art',
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/41o5B3nR1gL._SX329_BO1,204,203,200_.jpg',
-}
+const books = [
+    {
+        author: 'Hardcover – May 26, 2020',
+        title: 'Breath: The New Science of a Lost Art',
+        img:
+          'https://images-na.ssl-images-amazon.com/images/I/41o5B3nR1gL._SX329_BO1,204,203,200_.jpg',
+      },
+      {
+        author: 'Hardcover – October 20, 2020',
+        title: 'The Wim Hof Method: Activate Your Full Human Potential',
+        img:
+          'https://images-na.ssl-images-amazon.com/images/I/51ADdE1GjZL._SX329_BO1,204,203,200_.jpg',
+  },
+]
 
-const secoundBook = {
-  author: 'Hardcover – October 20, 2020',
-  title: 'The Wim Hof Method: Activate Your Full Human Potential',
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/51ADdE1GjZL._SX329_BO1,204,203,200_.jpg',
-}
-
+const names = ['John', 'Bruce', 'Wayn'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+});
 function BookList() {
-  return (
-    <section className='booklist'>
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      />
-      <Book
-        title={secoundBook.title}
-        author={secoundBook.author}
-        img={secoundBook.img}
-      >
-        <p>
-          "I invite you to witness your own being blossoming, to push past your
-          conditioning. This method is accessible to all."
-        </p>
-      </Book>
-    </section>
-  )
+  return <section className='booklist'>{newNames}</section>
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props
+  const { img, title, author } = props
   return (
     <article className='book'>
       <img src={img} alt='' height={300} />
-      {children}
       <h1>{title}</h1>
       <h4>{author}</h4>
     </article>
